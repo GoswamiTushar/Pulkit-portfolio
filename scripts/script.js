9522f58386c3cbb1b28a04b4af8e16077c68a825
GoswamiTushar <tushargoswami1510@gmail.com>
2021-07-18 21:03:00 +0530

    Modfication: added more projects

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,23 @@ var listitems = document.getElementById("list-items");
 
 const projectDesc = {
   "Developed Time Series Forecasting for Australian Monthly <br/>Gas Production":
-    "The project involves analysis of components of Time Series data, checking the seasonality fluctuations,monthly plot, decomposition of series, checking the stationarity of series,DE-seasonalise,decomposition of series, manual Arima(ACF & PACF Tests), Auto Arima, residual analysis.",
+    "The project involved analysis of components of Time Series data, checking the seasonality fluctuations, monthly plot, decomposition of series, checking the stationarity of series, DE-seasonalise, decomposition of series, manual Arima (ACF & PACF Tests), Auto Arima, residual analysis.",
+
+  "Visualizing Car Insurance Claims using Tableau":
+    "This project explored the art of problem-solving with the aid of visual analytics. Tableau’s data visualization tools were used to create interactive dashboards to provide high-level insights into an Insurance company to drive the company's car insurance schemes.",
+
+  "Choosing preferable mode of transport by employees":
+    "The project involves deciding on the mode of transport that the employees prefer while commuting to the office. For this, multiple models such as KNN, Naive Bayes, Logistic Regression have been created and explored to check their model performance metrics. Bagging and Boosting modeling procedures have also been applied to create the models.",
+
   "Market Segmentation in the context of Product <br/>Service Management":
     "The project involves performing Factor Analysis as well as Regression Analysis on a certain set of variables. It also requires to take independent variables and perform some correlation matrix to find out if there is any high correlation. Multiple Linear Regression has been performed by taking Customer Satisfaction as the dependent variable and the four factors as the independent variables.",
+
+  "Cellphone-Logistic project":
+    "The primary objective was to investigate the parameters contributing for customer churn (attrition) in the Telecom Industry. A Logistic Regression Model was developed and validated with test data to predict customer churn.",
+
+  "Theft Analytics for Discom":
+    "Worked on it by using predictive modelling techniques, data cleaning methods and various machine learning models with least misclassification errors while identifying defaulters and non-defaulters, in R & Python and prescribed the corrective actions to the respective DISCOM in order to reduce AT&C Losses.",
+
   "Evaluated the impact of change in temperature of Cold Storage products <br/>during the year":
     "The project computed the mean cold storage temperature in different seasons and used hypothesis testing to evaluate if any corrective action is required from the plant's side or the procurement's side.",
 };
@@ -33,6 +47,13 @@ const skillsAndTools = {
     "Factor Analysis, Multiple Linear Regression, Correlation Matrix, Market Segmentation",
   "Evaluated the impact of change in temperature of Cold Storage products <br/>during the year":
     "Descriptive Statistics, Inferential Statistics, Estimation & Hypothesis Testing, Predictive Modelling, Market Basket Analysis",
+  "Visualizing Car Insurance Claims using Tableau":
+    "Data Visualisation, Tableau, Business Intelligence",
+  "Choosing preferable mode of transport by employees":
+    "Bagging & Boosting, KNN, Naive Bayes, Logistic Regression",
+  "Cellphone-Logistic project":
+    "Logistic Regression, Model Comparison, Predictive Analytics",
+  "Theft Analytics for Discom": "NA",
 };
 const projectLinks = {};
 
